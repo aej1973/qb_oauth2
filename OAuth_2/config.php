607c150ff02240f86e687e85b507ebceeb2589b1
Arun AWS <you@example.com>
2017-08-01 04:18:24 +0000

    updated qb-oaath integration

--- OAuth_2/config.php
+++ OAuth_2/config.php
@@ -1,14 +1,15 @@
 <?php
 return array(
-  'authorizationRequestUrl' => '', //Example https://appcenter.intuit.com/connect/oauth2',
-  'tokenEndPointUrl' => '', //Example https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
-  'client_id' => '', //Example 'Q0wDe6WVZMzyu1SnNPAdaAgeOAWNidnVRHWYEUyvXVbmZDRUfQ',
-  'client_secret' => '', //Example 'R9IttrvneexLcUZbj3bqpmtsu5uD9p7UxNMorpGd',
-  'oauth_scope' => '', //Example 'com.intuit.quickbooks.accounting',
-  'openID_scope' => '', //Example 'openid profile email',
-  'oauth_redirect_uri' => '', //Example https://d1eec721.ngrok.io/OAuth_2/OAuth2PHPExample.php',
-  'openID_redirect_uri' => '',//Example 'https://d1eec721.ngrok.io/OAuth_2/OAuthOpenIDExample.php',
-  'mainPage' => '', //Example https://d1eec721.ngrok.io/OAuth_2/index.php',
-  'refreshTokenPage' => '', //Example https://d1eec721.ngrok.io/OAuth_2/RefreshToken.php'
+  'authorizationRequestUrl' => 'https://appcenter.intuit.com/connect/oauth2',
+  'tokenEndPointUrl' => 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
+  'client_id' => 'Q01JejXpcq8vSPdO8YAuTRWI9uiR0wi042us9o0eL2dZfRBBEP', //Example 'Q0wDe6WVZMzyu1SnNPAdaAgeOAWNidnVRHWYEUyvXVbmZDRUfQ',
+  'client_secret' => 'gHqe3WWRRAVKx2h2IJGw8Lck0XWpvUFHpRNQo2Hv', //Example 'R9IttrvneexLcUZbj3bqpmtsu5uD9p7UxNMorpGd',
+  'oauth_scope' => 'com.intuit.quickbooks.accounting', //Example 'com.intuit.quickbooks.accounting',
+  'openID_scope' => 'arun@nathanresearch.com',
+  'oauth_redirect_uri' => 'https://arunej.me/qb-oauth2/OAuth_2/OAuth2PHPExample.php/',
+  'openID_redirect_uri' => 'https://arunej.me/qb-oauth2/OAuth_2/OAuthOpenIDExample.php',
+  'mainPage' => 'https://arunej.me/qb-oauth2/OAuth_2/', //Example https://d1eec721.ngrok.io/OAuth_2/index.php',
+  'refreshTokenPage' => 'https://arunej.me/qb-oauth2/OAuth_2/RefreshToken.php'
 )
  ?>
+
--- OAuth_2/index.php
+++ OAuth_2/index.php
@@ -51,12 +51,13 @@ $refreshTokenPage = $configs['refreshTokenPage'];
     echo "<br /> <ipp:connectToIntuit></ipp:connectToIntuit><br />";
   }
 
-    echo "<h3>Please Complete the \"Sign In With Intuit\" flow:</h3>";
+/*    echo "<h3>Please Complete the \"Sign In With Intuit\" flow:</h3>";
     echo '<div> Add the OAuth 2 Consumer Key and OAuth 2 Consumer Secret of your application to config.php file to enable OpenID flow.</div> </br>
           <div> Add the openID_redirect_uri to config.php file. This URL is used by Intuit to redirect the user to your page when the user agreed for your app retrieving their personal information. </div> </br>
           <div> Click on the button below to start "Sign in with Intuit"</div>';
     //$loginStringGeneration = "<ipp:login href=\"" .$openID_redirect_uri . "\" type=\"horizontal\" ></ipp:login>";
     echo "<br /> <ipp:login href=\"" .$openID_redirect_uri . "\" type=\"horizontal\" ></ipp:login> <br />";
+*/
   //}
 
  ?>
